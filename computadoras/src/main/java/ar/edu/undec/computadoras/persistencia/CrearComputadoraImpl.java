68ff6f283fd8e738bccf4348261fb2b57c94b375
Emiliano <emiliano.peressini@gmail.com>
2020-11-10 09:34:52 -0300

    codificacion metodo guardado en la base de datos de la entidad computadora

--- computadoras/src/main/java/ar/edu/undec/computadoras/persistencia/CrearComputadoraImpl.java
+++ computadoras/src/main/java/ar/edu/undec/computadoras/persistencia/CrearComputadoraImpl.java
@@ -3,6 +3,7 @@ package ar.edu.undec.computadoras.persistencia;
 import ar.edu.undec.computadoras.core.dominio.Computadora;
 import ar.edu.undec.computadoras.core.repositorio.ICrearComputadoraRepositorio;
 import ar.edu.undec.computadoras.persistencia.crud.ICrearComputadoraCRUD;
+import ar.edu.undec.computadoras.persistencia.entity.ComputadoraEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class CrearComputadoraImpl implements ICrearComputadoraRepositorio {
@@ -16,6 +17,14 @@ public class CrearComputadoraImpl implements ICrearComputadoraRepositorio {
 
     @Override
     public boolean guardarComputadora(Computadora laComputadora) {
-        return false;
+        ComputadoraEntity computadoraBD = new ComputadoraEntity();
+        computadoraBD.setI310401000GB16GB("prueba 1");
+        computadoraBD.setS("prueba 2");
+        computadoraBD.setI(8);
+        computadoraBD.setSsd("SSD2");
+        computadoraBD.setI1(12);
+        computadoraBD.setPc("Pc 002");
+
+        return this.crearComputadoraCRUD.save(computadoraBD).getI310401000GB16GB()!=null;
     }
 }
